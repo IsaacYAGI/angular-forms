65671af222454680ef5b072024dce744506d2111
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-26 18:19:09 -0500

    Feat: Agrega validaciones en formulario

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -22,9 +22,9 @@ export class ReactiveComponent implements OnInit {
   crearFormulario(){
     this.forma = this.formBuilder.group({
       //valor por defecto // sync validator // async validator
-      nombre:[''],
-      apellido:[''],
-      correo:[''],
+      nombre:['', [Validators.required, Validators.minLength(5)]],
+      apellido:['', [Validators.required, Validators.minLength(5)]],
+      correo:['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
     });
   }
 
