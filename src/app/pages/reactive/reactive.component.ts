0e4c4a40befda2f983d685e64068ef7b42fc7ddf
Isaac Yriarte <isaacyagi@gmail.com>
2020-07-26 19:42:49 -0500

    Feat: Agrega elementos al formulario de forma dinamica

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -18,6 +18,10 @@ export class ReactiveComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  get pasatiempos(){
+    return this.forma.get('pasatiempos') as FormArray;
   }
 
   get nombreInvalido(){
@@ -49,7 +53,10 @@ export class ReactiveComponent implements OnInit {
       direccion:this.formBuilder.group({
         distrito: ['', Validators.required],
         ciudad: ['', Validators.required]
-      })
+      }),
+      pasatiempos: this.formBuilder.array([
+        [],[],[],[],[],
+      ])
     });
   }
 
